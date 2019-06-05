776595febe4c6eca1c2c22ad3bd5465922fac284
Yuanjun Yao <yjyao@cs.duke.edu>
2019-06-05 13:00:46 -0400

    update sh

--- prdn.sh
+++ prdn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/safe
-/root/sbt/bin/sbt  "project safe-server" "run -f /root/safe-apps/stitch-all.slang  -r safeService  -kd   src/main/resources/prdnsmall" 
+/root/sbt/bin/sbt  "project safe-server" "run -f /root/safe-multisdx/stitch-all.slang  -r safeService  -kd   src/main/resources/prdnsmall" 
--- sdx-routing.sh
+++ sdx-routing.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/safe
-/root/sbt/bin/sbt  "project safe-server" "run -f /root/safe-apps/sdx-routing.slang  -r safeService  -kd   src/main/resources/prdnsmall" 
+/root/sbt/bin/sbt  "project safe-server" "run -f /root/safe-multisdx/sdx-routing.slang  -r safeService  -kd   src/main/resources/prdnsmall" 
