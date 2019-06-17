56bb1fe41af993be9d00b36ee494af1b6e29d1cc
Yuanjun Yao <yyuanjun@vmware.com>
2019-06-17 09:36:11 -0700

    tridentcom2019 bench

---
+++ sdx-routing-client.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sbt "project safe-programming" "run -f /Users/yyuanjun/Downloads/uber-safe/safe-apps/safe-network/sdx/sdx-routing-client.slang   -jvmm src/main/resources/principal_jvm.map -c 1" 
